dc6577e1454f948edfd3848366f63d910089f31f
khantkyaw6 <khantkaneki@gmail.com>
2022-12-06 23:09:47 +0630

    redux toolkit

---
+++ src/icons.js
@@ -0,0 +1,67 @@
+export const CartIcon = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      className='h-6 w-6 '
+      fill='none'
+      viewBox='0 0 24 24'
+      stroke='currentColor'
+      strokeWidth={2}
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z'
+      />
+    </svg>
+  );
+};
+
+export const ChevronDown = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      className='h-6 w-6'
+      fill='none'
+      viewBox='0 0 24 24'
+      stroke='currentColor'
+      strokeWidth={2}
+    >
+      <path strokeLinecap='round' strokeLinejoin='round' d='M19 9l-7 7-7-7' />
+    </svg>
+  );
+};
+
+export const ChevronUp = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      className='h-6 w-6'
+      fill='none'
+      viewBox='0 0 24 24'
+      stroke='currentColor'
+      strokeWidth={2}
+    >
+      <path strokeLinecap='round' strokeLinejoin='round' d='M5 15l7-7 7 7' />
+    </svg>
+  );
+};
+
+export const Reap = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
+      stroke-width='1.5'
+      stroke='currentColor'
+      class='w-6 h-6'
+    >
+      <path
+        stroke-linecap='round'
+        stroke-linejoin='round'
+        d='M21 10.5h.375c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125H21M4.5 10.5H18V15H4.5v-4.5zM3.75 18h15A2.25 2.25 0 0021 15.75v-6a2.25 2.25 0 00-2.25-2.25h-15A2.25 2.25 0 001.5 9.75v6A2.25 2.25 0 003.75 18z'
+      />
+    </svg>
+  );
+};
